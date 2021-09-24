d0b1220cf00666f0ce57918ea29f55a7fda51e51
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-09-24 11:32:46 +0200

    Create septima_24_9.py

---
+++ kod_z_hodin/septima_24_9.py
@@ -0,0 +1,32 @@
+
+x = 5
+y = 15
+z = x + y
+
+a = "Moje vysledky:"
+b = "5"
+
+print(a)
+print("Z:", z)
+print(y - x)
+print(x, y, z)
+
+jmeno = input("Zadej jmeno:")
+print("Hello,", jmeno)
+
+# komentar
+
+# uloha - nacist cislo od uzivatele, spocitat druhou mocninu, vypsat vysledek
+
+cislo = input("Zadej cislo")
+#cislo = int(input("Zadej cislo"))
+cislo = int(cislo)
+mocnina = cislo ** 2
+print(cislo, "na druhou je", mocnina)
+
+# datove typy
+# cele cislo - integer - int
+# desetinne - float
+# retezec - slovo - string - str
+
+# funkce na prevadeni - int(), str()
