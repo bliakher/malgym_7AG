429263adc4c25c18bd091a6e08a2d3c4377727f9
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-03-04 11:40:37 +0100

    Add files via upload

---
+++ kod_z_hodin/septima_4_3.py
@@ -0,0 +1,39 @@
+
+x = 5
+y = 7
+a = "ahoj" # retezec - string
+
+z = x + y
+z = z * 2
+
+"""
+print(z)
+print("Ahoj svete!")
+print(x, y, z)
+
+cislo = int(input("Napis cislo: "))
+print(cislo ** 2)
+"""
+
+x = 2
+x = "2"
+
+# int() , str()
+
+a = int(input("Napis cislo: "))
+b = int(input("Napis cislo: "))
+if b != 0:
+    print(a // b)
+else:
+    print("nelze delit nulou")
+    
+# == , !=, <, >, <=, >=
+
+o = '+'
+if o == '+':
+    pass
+else:
+    if o == '-':
+        pass
+    else:
+        pass
