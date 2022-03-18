045754f68b7baa6daa3352f49fbb95513adb7bf6
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-03-18 11:41:33 +0100

    Add files via upload

---
+++ kod_z_hodin/septima_18_3.py
@@ -0,0 +1,52 @@
+
+print("Hello world!")
+
+print(5+7)
+
+x = 5
+y = 2
+z = x + y
+
+print(z)
+
+ahoj444 = 444
+
+"""
+a = input("Napis neco: ")
+print(a)
+
+
+jmeno = input("Jmeno:")
+print("Hello,", jmeno)
+"""
+# komentar
+
+
+a = 5 #integer - int
+b = "hello444" # retezec - string - str
+
+#jmeno = input("Jmeno:") # "Evgenia"
+"""
+cislo1 = int(input("Cislo:"))
+cislo2= int(input("Cislo:"))
+
+print(cislo1, "+", cislo2, "=", cislo1+cislo2)
+"""
+
+
+a = int(input("Cislo:"))
+b = int(input("Cislo:"))
+
+if b == 0:
+    print("nelze delit")
+else:
+    print(a / b)
+    print(a // b)
+
+
+
+
+
+
+
+
