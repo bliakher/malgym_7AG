bd4191f73faaf98be6ad7821aa4523b71618725f
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-03-25 11:30:11 +0100

    Add files via upload

---
+++ kod_z_hodin/septima_25_3.py
@@ -0,0 +1,73 @@
+"""
+print("Hello world")
+
+print(42)
+
+x = 2
+y = 5
+z = x + y
+
+print(z)
+
+hello = "Hello"
+
+print(hello)
+
+name = input("Write your name:")
+print("Hello,", name)
+
+
+num = int(input("number:")) # int - integer
+print(num * 5)
+"""
+# komentar
+
+
+# < > <= >= == !=
+
+# + - * / // %
+
+"""
+print(13 / 7)
+print(13 // 7)
+print(13 % 7)
+
+x = int(input("cislo:"))
+
+if x >= 0:
+    if x == 0:
+        print("nula")
+    else:
+        print("kladne")
+else:
+    print("zaporne")
+"""
+
+# cyklus while
+
+x = 1
+while x <= 10:
+    print(x)
+    x = x + 1 # x += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
