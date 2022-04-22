3cec87ba87a663a654519a9b6c77558966a0a533
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-04-22 11:31:39 +0200

    Add files via upload

---
+++ kod_z_hodin/septima_22_4.py
@@ -0,0 +1,49 @@
+# FizzBuzz
+# napoveda pro jedno cislo
+
+
+
+# pole - seznam
+
+x = 5 # promenna
+"""
+stud = "Eliska"
+stud2 = "Honza"
+stud3 = "Martin"
+#..
+
+print(stud)
+print(stud2)
+print(stud3)
+"""
+
+studenti = ["Eliska", "Honza", "Martin"]
+eliska = studenti[0]
+print(eliska)
+
+print(studenti[2])
+
+cisla = [1, 2, 3, 4, 5]
+mix = [1, "ahoj", True]
+
+
+print(studenti)
+studenti.append("Jasen")
+print(studenti)
+
+# velikost - pocet prvku
+print("pole studenti ma", len(studenti), "studenty")
+
+
+cisla = [1, 2, 3, 4, 5]
+
+print() # prvni
+print() # posledni
+
+
+
+
+
+
+
+
