200a7bc81387e0d37d327dc0ca2f40c53b77f2ed
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-06-10 11:13:42 +0200

    Add files via upload

---
+++ kod_z_hodin/septima_10_6.py
@@ -0,0 +1,17 @@
+
+import random as rnd
+import time
+
+a = rnd.randint(1, 6)
+print(a)
+
+
+barvy = ["cervena", "modra", "zelena"]
+
+b = rnd.choice(barvy)
+print(b)
+
+time.sleep(3)
+
+print("hello")
+
